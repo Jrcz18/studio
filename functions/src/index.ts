69140e5eaead4d4acd3e1477be7b132cdf4ba147
Jeric Patiag <jericpatiag16@gmail.com>
2025-09-24 11:22:16 +0000

    > functions@1.0.0 build > tsc
    src/index.ts:59:38 - error TS7030: Not al

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -184,5 +184,4 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
     res.status(500).json({ error: 'Something went wrong!', message: err.message });
 });
 
-const functionsV1 = functions.region('asia-southeast1');
-export const api = functionsV1.https.onRequest(app);
+export const api = functions.region('asia-southeast1').https.onRequest(app);
